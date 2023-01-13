c7984f1c2dc2d4e4700c82f3eae3a40eb7675bde
jerrymis <1831433004@qq.com>
2023-01-13 20:09:28 +0800

    partly finished

---
+++ lab/lab06/lab06.py
@@ -0,0 +1,97 @@
+this_file = __file__
+
+
+def make_adder_inc(a):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2)
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    def plus(num):
+        nonlocal a
+        a += 1
+        return a + num -1
+    return plus
+
+
+def make_fib():
+    """Returns a function that returns the next Fibonacci number
+    every time it is called.
+
+    >>> fib = make_fib()
+    >>> fib()
+    0
+    >>> fib()
+    1
+    >>> fib()
+    1
+    >>> fib()
+    2
+    >>> fib()
+    3
+    >>> fib2 = make_fib()
+    >>> fib() + sum([fib2() for _ in range(5)])
+    12
+    >>> from construct_check import check
+    >>> # Do not use lists in your implementation
+    >>> check(this_file, 'make_fib', ['List'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    cnt = 0
+    num1 = 0
+    num2 = 1
+    def Fib_num():
+        nonlocal num1,num2,cnt
+        if cnt <= 1:
+            cnt += 1
+            return cnt - 1
+        else:
+            cnt += 1
+            tmp = num2
+            num2 = num1 + num2
+            num1 = tmp
+            return num2
+    return Fib_num
+
+
+
+def insert_items(lst, entry, elem):
+    """
+    >>> test_lst = [1, 5, 8, 5, 2, 3]
+    >>> new_lst = insert_items(test_lst, 5, 7)
+    >>> new_lst
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> large_lst = [1, 4, 8]
+    >>> large_lst2 = insert_items(large_lst, 4, 4)
+    >>> large_lst2
+    [1, 4, 4, 8]
+    >>> large_lst3 = insert_items(large_lst2, 4, 6)
+    >>> large_lst3
+    [1, 4, 6, 4, 6, 8]
+    >>> large_lst3 is large_lst
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    cnt = -1
+    flag = False
+    for each in lst:
+        cnt += 1 
+        if flag == True:
+            flag = False
+            continue
+        if each == entry:
+            lst.insert(cnt+1,elem)
+            flag = True
+    return lst
+
